75f75b833ace4d56c96ba7bd2edd1ed0fc065141
haru-voster <harounbravo2@gmail.com>
2023-03-28 15:26:50 -0700

    printstring

--- print_string.c
+++ print_string.c
@@ -1,20 +1,20 @@
 #include "main.h"
-
-/**
- * print_string - Print string
- * @list: list.
- *
- * Return: String length.
- */
-
-int print_string(va_list list)
 {
-	char *p;
-	int p_len;
+    const unsigned char *p = (const unsigned char *) str;
+    while (*p) {
+        if (*p >= 32 && *p < 127) {
+            putchar(*p);
+        } else {
+            printf("\\x%02X", *p);
+        }
+        p++;
+    }
+}
 
-	p = va_arg(list, char*);
-	p_len = print((p != NULL) ? p : "(null)");
-
-	return (p_len);
+int main() {
+    const char *str = "Hello, world!\n\x1b[31mThis is a red text\x1b[0m.";
+    printf("String with non-printable characters: %S\n", str);
+    printf("String without special treatment: %s\n", str);
+    return 0;
 }
 
